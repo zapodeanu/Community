d9838425e1374292af4ccdfde4772387a81503c7
gzapodea <gzapodea@cisco.com>
2017-04-24 21:23:42 -0700

    work in progress

--- Spark_APIs_Messages.py
+++ Spark_APIs_Messages.py
@@ -7,14 +7,23 @@
 import requests
 import json
 import requests.packages.urllib3
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-from requests.auth import HTTPBasicAuth  # for Basic Auth
-
 import Spark_APIs_init
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Disable insecure https warnings
 
 from Spark_APIs_init import SPARK_AUTH, SPARK_URL
 
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Disable insecure https warnings
+
+def pprint(json_data):
+    """
+    Pretty print JSON formatted data
+    :param json_data:
+    :return:
+    """
+
+    print(json.dumps(json_data, indent=4, separators=(' , ', ' : ')))
+
 
 
 
