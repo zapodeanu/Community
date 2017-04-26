11347b9e64059b33a665e2729caa50e265b6f841
Gabriel Zapodeanu <gzapodea@cisco.com>
2017-04-26 16:06:14 -0700

    Create Meraki_APIs_init.py

---
+++ Meraki_APIs_init.py
@@ -0,0 +1,10 @@
+
+
+# developed by Gabi Zapodeanu, TSA, GSS, Cisco Systems
+
+# !/usr/bin/env python3
+
+
+MERAKI_API_KEY = 'Put your Meraki API key here'
+MERAKI_ORG_ID = 'Put your Meraki Organization Id here'
+MERAKI_URL = 'https://dashboard.meraki.com/api/v0'
